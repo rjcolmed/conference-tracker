867b539d2d85c34e87d0c5ac904a6978e0af9f59
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-09-02 17:21:32 -0400

    import mock data into app.js

--- src/components/layout/App.js
+++ src/components/layout/App.js
@@ -2,12 +2,16 @@ import m from 'mithril'
 import Container from './Container'
 import NavBar from './NavBar'
 
+import { CONFERENCES } from '../../store/data'
+
 
 const App = {
-  view: () => m('div', [
-    m(Container),
-    m(NavBar)
-  ])
+  view: () => { 
+    return m('div', [
+      m(Container, { conferences: CONFERENCES }),
+      m(NavBar)
+    ])
+}
 }
 
 export default App
