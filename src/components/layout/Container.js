ab985d558abf4f1fc917cf6b032cc78b0c00bea8
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-09-02 17:21:02 -0400

    use CardContainer and ConferenceCard components in Container.js

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,11 +1,16 @@
 import m from 'mithril'
+import CardContainer from './CardContainer'
+import ConferenceCard from '../cards/ConferenceCard'
 
 import Banner from '../ui/Banner'
 
 const Container = {
-  view: () => {
+  view: ({ attrs }) => {
     return m('div', [
-      m(Banner)
+      m(Banner, { action: () => console.log('clicked'), title: 'Conferences' }),
+      m(CardContainer, attrs.conferences.map((conference, key) => {
+        return m(ConferenceCard, { conference, key })
+      }))
     ])
   }
 }
